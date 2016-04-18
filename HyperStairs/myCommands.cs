bc7e532f804683850365a32ef49f9a650dff4428
Mike <Mike@MIKE-PC>
2016-04-19 01:43:54 +0300

    Deleted some useless parts of code

--- HyperStairs/myCommands.cs
+++ HyperStairs/myCommands.cs
@@ -1,12 +1,11 @@
-﻿// (C) Copyright 2016 by  
-//
-
-using System;
+﻿using System;
+using System.Windows;
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
 
 // This line is not mandatory, but improves loading performances
 
@@ -31,6 +30,7 @@ namespace HyperStairs
         // context menu.
 
         // Modal Command with localized name
+        /*
         [CommandMethod("MyGroup", "MyCommand", "MyCommandLocal", CommandFlags.Modal)]
         public void MyCommand() // This method can have any name
         {
@@ -44,6 +44,7 @@ namespace HyperStairs
                 ed.WriteMessage("Hello, this is your first command with argument: " + pr.StringResult);
             }
         }
+        */
 
         [CommandMethod("CreateStairs")]
         public static void CreateStairs()
@@ -78,15 +79,6 @@ namespace HyperStairs
                 AddLine(new Point3d(0, 0, 0), new Point3d(w, 0, 0), tr, btr);
                 AddLine(new Point3d(0, l, h), new Point3d(w, l, h), tr, btr);
 
-                //ObjectId btrId = bt.Add(btr);
-               // BlockReference br = new BlockReference(Point3d.Origin, btrId);
-
-              //   BlockTableRecord ms =
-               //     (BlockTableRecord) tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
-
-               //  ms.AppendEntity(btr);
-             //    tr.AddNewlyCreatedDBObject(br, true);
-
                 // Сохранение нового объекта в базе данных
                 tr.Commit();
             }
@@ -98,42 +90,6 @@ namespace HyperStairs
             l.SetDatabaseDefaults();
             r.AppendEntity(l);
             t.AddNewlyCreatedDBObject(l, true);
-        }
-
-// Modal Command with pickfirst selection
-        [CommandMethod("MyGroup", "MyPickFirst", "MyPickFirstLocal", CommandFlags.Modal | CommandFlags.UsePickSet)]
-        public void MyPickFirst() // This method can have any name
-        {
-            PromptSelectionResult result = Application.DocumentManager.MdiActiveDocument.Editor.GetSelection();
-            if (result.Status == PromptStatus.OK)
-            {
-                // There are selected entities
-                // Put your command using pickfirst set code here
-            }
-            else
-            {
-                // There are no selected entities
-                // Put your command code here
-            }
-        }
-
-        // Application Session Command with localized name
-        [CommandMethod("MyGroup", "MySessionCmd", "MySessionCmdLocal", CommandFlags.Modal | CommandFlags.Session)]
-        public void MySessionCmd() // This method can have any name
-        {
-            // Put your command code here
-        }
-
-        // LispFunction is similar to CommandMethod but it creates a lisp 
-        // callable function. Many return types are supported not just string
-        // or integer.
-        [LispFunction("MyLispFunction", "MyLispFunctionLocal")]
-        public int MyLispFunction(ResultBuffer args) // This method can have any name
-        {
-            // Put your command code here
-
-            // Return a value to the AutoCAD Lisp Interpreter
-            return 1;
         }
     }
 }
