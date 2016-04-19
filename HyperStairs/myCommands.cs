522829d1a20866ad95b753b4c47862a9784a8327
Mike <Mike@MIKE-PC>
2016-04-19 22:45:51 +0300

    Added block sample

--- HyperStairs/myCommands.cs
+++ HyperStairs/myCommands.cs
@@ -13,10 +13,6 @@ using Application = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace HyperStairs
 {
-    // This class is instantiated by AutoCAD for each document when
-    // a command is called by the user the first time in the context
-    // of a given document. In other words, non static data in this class
-    // is implicitly per-document!
     public class MyCommands
     {
         // The CommandMethod attribute can be applied to any public  member 
@@ -30,21 +26,6 @@ namespace HyperStairs
         // context menu.
 
         // Modal Command with localized name
-        /*
-        [CommandMethod("MyGroup", "MyCommand", "MyCommandLocal", CommandFlags.Modal)]
-        public void MyCommand() // This method can have any name
-        {
-            // Put your command code here
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Editor ed;
-            if (doc != null)
-            {
-                ed = doc.Editor;
-                PromptResult pr = ed.GetString("Enter string, please");
-                ed.WriteMessage("Hello, this is your first command with argument: " + pr.StringResult);
-            }
-        }
-        */
 
         [CommandMethod("CreateStairs")]
         public static void CreateStairs()
@@ -90,6 +71,146 @@ namespace HyperStairs
             l.SetDatabaseDefaults();
             r.AppendEntity(l);
             t.AddNewlyCreatedDBObject(l, true);
+        }
+
+        [CommandMethod("CB")]
+        public void CreateBlock()
+        {
+            Document doc =
+                Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            Transaction tr =
+                db.TransactionManager.StartTransaction();
+            using (tr)
+            {
+                // Get the block table from the drawing
+
+                BlockTable bt =
+                    (BlockTable) tr.GetObject(
+                        db.BlockTableId,
+                        OpenMode.ForRead
+                        );
+
+                // Check the block name, to see whether it's
+                // already in use
+
+                PromptStringOptions pso = new PromptStringOptions("\nEnter new block name: ");
+                pso.AllowSpaces = true;
+
+                // A variable for the block's name
+
+                string blkName = "";
+
+                do
+                {
+                    PromptResult pr = ed.GetString(pso);
+
+                    // Just return if the user cancelled
+                    // (will abort the transaction as we drop out of the using
+                    // statement's scope)
+
+                    if (pr.Status != PromptStatus.OK)
+                        return;
+
+                    try
+                    {
+                        // Validate the provided symbol table name
+
+                        SymbolUtilityServices.ValidateSymbolName(
+                            pr.StringResult,
+                            false
+                            );
+
+                        // Only set the block name if it isn't in use
+
+                        if (bt.Has(pr.StringResult))
+                            ed.WriteMessage("\nA block with this name already exists.");
+                        else
+                            blkName = pr.StringResult;
+                    }
+                    catch
+                    {
+                        // An exception has been thrown, indicating the
+                        // name is invalid
+
+                        ed.WriteMessage("\nInvalid block name.");
+                    }
+                } while (blkName == "");
+
+                // Create our new block table record...
+
+                BlockTableRecord btr = new BlockTableRecord();
+
+                // ... and set its properties
+
+                btr.Name = blkName;
+
+                // Add the new block to the block table
+
+                bt.UpgradeOpen();
+                ObjectId btrId = bt.Add(btr);
+                tr.AddNewlyCreatedDBObject(btr, true);
+
+                // Add some lines to the block to form a square
+                // (the entities belong directly to the block)
+
+                DBObjectCollection ents = SquareOfLines(5);
+                foreach (Entity ent in ents)
+                {
+                    btr.AppendEntity(ent);
+                    tr.AddNewlyCreatedDBObject(ent, true);
+                }
+
+                // Add a block reference to the model space
+
+                BlockTableRecord ms =
+                    (BlockTableRecord) tr.GetObject(
+                        bt[BlockTableRecord.ModelSpace],
+                        OpenMode.ForWrite
+                        );
+
+                BlockReference br =
+                    new BlockReference(Point3d.Origin, btrId);
+
+                ms.AppendEntity(br);
+                tr.AddNewlyCreatedDBObject(br, true);
+
+                // Commit the transaction
+
+                tr.Commit();
+
+                // Report what we've done
+
+                ed.WriteMessage(
+                    "\nCreated block named \"{0}\" containing {1} entities.",
+                    blkName, ents.Count
+                    );
+            }
+        }
+
+        private DBObjectCollection SquareOfLines(double size)
+        {
+            // A function to generate a set of entities for our block
+
+            DBObjectCollection ents = new DBObjectCollection();
+            Point3d[] pts =
+            {
+                new Point3d(-size, -size, 0),
+                new Point3d(size, -size, 0),
+                new Point3d(size, size, 0),
+                new Point3d(-size, size, 0)
+            };
+            int max = pts.GetUpperBound(0);
+
+            for (int i = 0; i <= max; i++)
+            {
+                int j = (i == max ? 0 : i + 1);
+                Line ln = new Line(pts[i], pts[j]);
+                ents.Add(ln);
+            }
+            return ents;
         }
     }
 }
